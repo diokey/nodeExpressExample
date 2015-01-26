a64c4e3a3d08055d2364bb73173d89c1be7356c6
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-01-25 21:38:40 -0500

    Added login form

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ var app = express();
 app.set('view engine','ejs');
 
 app.get('/',function(req, res) {
-    res.render('index');
+    res.render('index',{
+        isAuthenticated : false,
+        user : null
+    });
+});
+
+app.get('/login',function(res,res) {
+    res.render('login');
 });
 
 var port = process.env.PORT || 1337;
